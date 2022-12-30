6274a76986f9344bbf4d1ff5d9a2f1ed65360b17
Ho Thi Phuong <phuong.ho23@student.passerellesnumeriques.org>
2022-12-30 09:21:49 +0700

    First

---
+++ src/test/java/runner/RunCusBank.java
@@ -0,0 +1,14 @@
+package runner;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/resources/features/testbankCustomerLogin.feature",
+        plugin = { "json:target/cucumber.json", "pretty", "html:target/cucumber-reports" }
+)
+class RunCusBank {
+}
